9b7e437128e5bfee54381eef945be5e9d67c2007
ROHAN <krohan1202@gmail.com>
2021-08-18 12:53:15 +0530

    Add delete feature

--- redux-reduxtoolkit-prac-jk/src/components/Todo.js
+++ redux-reduxtoolkit-prac-jk/src/components/Todo.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 
-import { todoTask } from "../features/todo";
+import { todoTask, todoTaskRemover } from "../features/todo";
 
 function Todo() {
     const dispatch = useDispatch();
@@ -17,10 +17,18 @@ function Todo() {
     return (
         <div>
             {todoTasks.taskName  && todoTasks.taskName.map((t) => (
-                <li>{t}</li>
+                <div style={{'margin': '1vw 0'}}>
+                <li style={{'display': 'inline'}}>{t}</li>
+                <button value={t} style={{'marginLeft': '3vw'}} onClick={()=> {
+                    dispatch(todoTaskRemover({t}));
+                }}>Delete</button>
+                </div>
             ))}
-            <input onChange={(e) => setText(e.target.value)}/>
-            <button onClick={() => dispatch(todoTask(text))}>Go</button>
+            <input value={text} onChange={(e) => setText(e.target.value)}/>
+            <button type="submit" onClick={() => {
+                setText(""); //Not working
+                dispatch(todoTask(text));
+            }}>Go</button>
         </div>
     )
 }
--- redux-reduxtoolkit-prac-jk/src/features/todo.js
+++ redux-reduxtoolkit-prac-jk/src/features/todo.js
@@ -11,10 +11,18 @@ export const todoSlice = createSlice({
     reducers: {
         todoTask: (state, action) => {
             state.value.taskName.push(action.payload);
+        },
+        todoTaskRemover: (state, action) => {
+            console.log(action.payload.t);
+            // Getting a certain element from taskName array
+            const index = state.value.taskName.indexOf(action.payload.t);
+            if (index > -1) {
+                state.value.taskName.splice(index, 1);
+            }           
         }
     }
 })
 
-export const {todoTask} = todoSlice.actions;
+export const { todoTask, todoTaskRemover } = todoSlice.actions;
 
 export default todoSlice.reducer;
