817758aba4c36ad9c396ae47e637dff78559810f
ROHAN <krohan1202@gmail.com>
2021-08-23 13:46:08 +0530

    Add todo.css

--- redux-reduxtoolkit-prac-jk/src/components/Todo.js
+++ redux-reduxtoolkit-prac-jk/src/components/Todo.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "../styles/todo.css";
 import { useDispatch, useSelector } from "react-redux";
 
 import { todoTask, todoTaskRemover } from "../features/todo";
